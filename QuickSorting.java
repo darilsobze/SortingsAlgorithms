688b1507ef6d42c4128315d020144a504d0048c9
darilsobze <darilsobze13@icloud.com>
2024-06-21 11:33:54 +0200

    QuickSorting

--- QuickSorting.java
+++ QuickSorting.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class QuickSorting {
+public class QuickSorting {
+
+    public static void quickSort( int[] tab, int left, int right){
+        if (left<right){
+            int q = partition(tab, left, right);
+            quickSort(tab, left,q);
+            quickSort(tab,q+1, right);
+        }
+    }
+
+    public static int partition( int[] tab, int left, int right){
+        int pivot = tab[left];
+        int p = left -1;
+        int q = right+1;
+        while (p<q){
+           do {
+               p++;
+           }
+           while (tab[p]< pivot);
+           do {
+               q--;
+           }
+           while (tab[q]> pivot);
+            if (p<q)
+                Sorting.swap(tab,p,q);
+        }
+        return q;
+    }
 }
