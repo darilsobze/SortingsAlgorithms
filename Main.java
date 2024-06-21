69bce5b982c08622d1b8f719d39f222d9683045c
darilsobze <darilsobze13@icloud.com>
2024-06-21 11:35:42 +0200

    Main

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
+import java.util.Queue;
 
 public class Main {
 
     public static void main( String[] args){
         System.out.println("Hello World !!!");
         System.out.println("There are a lot of Sorting's Algorithms");
-        int[] array = new int[]{30,12,20,9,15};
-        SelectionSorting.selectionSort(array);
+        int[] array = new int[]{30,12,20,9,15,17,58,47,77,1,3};
+        //SelectionSorting.selectionSort(array);
+        //MergeSorting.mergeSort(array,0,10);
+        QuickSorting.quickSort(array,0,10);
         System.out.println(Arrays.toString(array));
     }
 }
